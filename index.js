9d2a284a3de4c14c5a090d7d1428da309c0ddaac
Nezam Uddin <3964864+Nezam@users.noreply.github.com>
2019-08-16 17:26:50 +0300

    working alhumdulillah

--- index.js
+++ index.js
@@ -3,24 +3,50 @@ const path = require('path');
 var request = require('request');
 const http = require('https');
 const fs = require('fs');
+const Telegraf = require('telegraf');
+const Telegram = require('telegraf/telegram');
 const PORT = process.env.PORT || 5000
-
+var _nameOfFile="";
 const app = express();
+var BOT_TOKEN='452553647:AAGGiKN-rsQrORgJNdNL63lDDQ549aFKFo0';
+var FAWZAN_CHATID='@shalfawzan';
+var ADMIN_CHATID='@NezamUddin';
 
 
-const tUpload = function(msg){
-    console.log("CB WAS CALLED");
-    if(msg!=undefined){
-    console.error(msg);
-    }
+const botWork = function(dest,jsonResponse){
+const bot = new Telegraf(BOT_TOKEN)
+bot.launch();
+    const extra = {
+    ...jsonResponse.info.title && { title: jsonResponse.info.title },
+          
+              ...jsonResponse.info.duration && { duration: jsonResponse.info.duration },
+              ...jsonResponse.info.creator && { performer: jsonResponse.info.creator },
+              ...jsonResponse.info.description && { caption: '@shalfawzan' },
+};
+    bot.telegram.sendAudio(FAWZAN_CHATID,{ source: dest },extra);
+    if(jsonResponse.info.description!=null && jsonResponse.info.description!=undefined)
+    bot.telegram.sendMessage(FAWZAN_CHATID,jsonResponse.info.description);
+    //delete from server
+    fs.unlink(dest, (err) => {
+  if (err) {
+    console.error(err)
+    return
+  }});
+
 }
-const downloadFile = function(url,dest,cb){
+
+
+const downloadFile = function(url,dest,jsonResponse){
    request
   .get(url)
   .on('error', function(err) {
     // handle error
   })
-  .pipe(fs.createWriteStream(dest));
+  .pipe(fs.createWriteStream(dest))
+   .on('finish',function(args){
+       console.log('enterred on finish');
+      botWork(dest,jsonResponse);
+   });
 }
 
 
@@ -42,21 +68,16 @@ app
             var fileName = jsonRes.info.title;
             fileName = fileName.replace(/ /g,"_");
             fileName = fileName+"."+jsonRes.info.ext;
+            nameOfFile = fileName;
             fileName = __dirname+"/"+fileName;
             console.log("full file name:"+fileName);
-            downloadFile(jsonRes.info.url,fileName,tUpload);
+            downloadFile(jsonRes.info.url,fileName,jsonRes);
         }
     })
     res.end();
 });
 
-    
 
-/*app
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))*/
 
 
 //start server
