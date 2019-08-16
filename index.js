0ffe0b7b56a49263adbfc2f24bea038bd049cec5
Nezam Uddin <3964864+Nezam@users.noreply.github.com>
2019-08-16 10:11:28 +0300

    working

--- index.js
+++ index.js
@@ -15,41 +15,13 @@ const tUpload = function(msg){
     }
 }
 const downloadFile = function(url,dest,cb){
-    console.log("starting download to "+dest+"\n \n URL:"+url);
-    var file = fs.createWriteStream(dest);
-  var requ = http.get(url, function(response) {
-    response.pipe(file);
-    file.on('finish', function() {
-        console.log("reached finished");
-      file.end(cb);  // close() is async, call cb after close completes.
-    });
-      // check for request error too
-    requ.on('error', (err) => {
-        fs.unlink(dest);
-        return cb(err.message);
-    });
-
-    file.on('error', (err) => { // Handle errors
-        fs.unlink(dest); // Delete the file async. (But we don't check the result) 
-        return cb(err.message);
-    });
-  }).on('error', function(err) { // Handle errors
-    fs.unlink(dest); // Delete the file async. (But we don't check the result)
-    if (cb) cb(err.message);
-  });
-};
-
-  
-/*const getVideo = (request,response) =>{
-    var urls = request.url; 
-    
-    //get youtube id and make request to youtube-dl http and get json
-    console.log(urls.pathname);
-    //request("https://salafi.herokuapp.com/api/info?url=https://www.youtube.com/watch?v=" + _ytlink + "&format=bestaudio")
-    //download the best-audio format from json to local folder
-    //send the downloaded audio to telegram chat
-
-}*/
+   request
+  .get(url)
+  .on('error', function(err) {
+    // handle error
+  })
+  .pipe(fs.createWriteStream(dest));
+}
 
 
 app
@@ -64,9 +36,9 @@ app
     console.log("SUCCESS. Video:"+req.params.vidId);
     request.get("https://salafi.herokuapp.com/api/info?url=https://www.youtube.com/watch?v=" + req.params.vidId + "&format=bestaudio",function(error,response,body){
         if(!error && response.statusCode==200){
-            console.log(body);
+            //console.log(body);
             var jsonRes = JSON.parse(body);
-            console.log("PARSED URL:"+jsonRes.info.url);
+           // console.log("PARSED URL:"+jsonRes.info.url);
             var fileName = jsonRes.info.title;
             fileName = fileName.replace(/ /g,"_");
             fileName = fileName+"."+jsonRes.info.ext;
